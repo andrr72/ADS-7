b876895ecaa849de53175963f863fc0874504cef
Andrei <140070906+andrr72@users.noreply.github.com>
2025-05-18 13:19:24 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,45 +1,62 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+#include <stdexcept>
 
 Train::Train() {
-  countOp = 0;
   first = nullptr;
+  countOp = 0;
+}
+
+Train::~Train() {
+  if (!first) return;
+  Car* current = first->next;
+  while (current != first) {
+    Car* temp = current;
+    current = current->next;
+    delete temp;
+  }
+  delete first;
 }
 
 void Train::addCar(bool light) {
   Car* newCar = new Car{light, nullptr, nullptr};
   if (!first) {
+    newCar->next = newCar->prev = newCar;
     first = newCar;
-    newCar->next = newCar;
-    newCar->prev = newCar;
   } else {
     Car* last = first->prev;
-    newCar->next = first;
-    newCar->prev = last;
-    first->prev = newCar;
     last->next = newCar;
+    newCar->prev = last;
+    newCar->next = first;
+    first->prev = newCar;
   }
 }
 
 int Train::getLength() {
-  if (!first) return 0;
-  if (first->next == first) return 1;
-  
-  Car* current = first;
-  current->light = false;
-  int length = 1;
-  bool firstCycle = true;
-  
+  Car* start = first;
+  Car* current = start;
+  if (!current->light)
+    current->light = true;
+  int count = 1;
+  current = current->next;
+  countOp++;
   while (true) {
-    current = current->next;
-    countOp++;
-    if (!current->light) {
-      if (current == first && !firstCycle) {
-        return length;
-      }
+    if (current->light) {
       current->light = false;
-      length++;
-      firstCycle = false;
+      for (int i = 0; i < count; ++i) {
+        current = current->prev;
+        countOp++;
+      }
+      if (!current->light) {
+        return count;
+      }
+      current = current->next;
+      countOp++;
+      count = 1;
+    } else {
+      current = current->next;
+      countOp++;
+      count++;
     }
   }
 }
